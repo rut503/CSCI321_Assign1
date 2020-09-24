39065d16870a551b318d07351a6ff6fbfc00576c
hardik patel <hp9423605066@gmail.com>
2020-09-24 12:28:03 -0500

    got button and text field working with error message

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ageInYears: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ageButton() {
+        
+        ageInYears.resignFirstResponder()
+        guard let age = ageInYears.text, !age.isEmpty else {
+            alert( message: "Please enter Dog's Age.")
+            return
+        }
+        
+        var numericAge = Int(age)!
+        numericAge = numericAge + 15
+        
+        outputLabel.text = "Your dog is \(numericAge) in Human Years."
+    }
+    
+    func alert( message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
